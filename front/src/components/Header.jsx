7deb91720c926f648c8a2dc0096b92b1dd1f160d
S7-Enva <meneve.kevin@gmail.com>
2022-10-14 11:56:01 +0200

    Mise en place du logo

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
+import logo from "../assets/LOGO.png"
  
 function Header() {
     return (
         <header>
-            <img className="Header__logo" src="../../public/LOGO.png" alt="Logo Kasa" />
+            <img className="Header__logo" src={logo} alt="Logo Kasa" />
             <nav>
                 <Link to="/">Accueil</Link>
                 <Link to="/About">A propos</Link>
