2ed7dfb7fe1b777d6708fd3cc2214e1174fb58f9
S7-Enva <meneve.kevin@gmail.com>
2022-10-16 11:03:20 +0200

    Mise en place de la page erreur et du footer

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/LOGO.png"
+ 
+function Footer() {
+    return (
+        <footer>
+            <img className="Header__logo" src={logo} alt="Logo Kasa" />
+        </footer>
+    )
+}
+
+export default Footer
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Error from './pages/Error'
 import './sass/style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,7 +16,9 @@ root.render(
       <Routes>
         <Route exact path="/" element = {<Home />}/>
         <Route path="/About" element = {<About />}/>
+        <Route path="/*" element = {<Error />}/>
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
